466ff0f16fdaf3cab97a66c4aed6762b5908aab7
Michele <michele.pellegrino@scilifelab.se>
2024-03-14 16:05:11 +0100

    Updates and refactoring

--- comparison.py
+++ comparison.py
@@ -2,6 +2,7 @@ import numpy as np
 import numpy.random as rng
 
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
 import scipy.optimize as sc_opt
 import scipy.special as sc_spc
@@ -14,169 +15,251 @@ tan_m1 = lambda t : np.rad2deg(np.arctan(t))
 cos_m1 = lambda t : np.rad2deg(np.arccos(t))
 sin_m1 = lambda t : np.rad2deg(np.arcsin(t))
 
-### REDUCED UNITS ###
-
-# Droplet radius [nm]
-R0 = 25.0
-print("Droplet radius         = "+str(R0)+" [nm]")
-# Corrugation spacing [1/nm]
-k = 360.0/4.0
-print("Corrugation spacing    = "+str(k)+" [1/nm]")
-# Displacement prefactor [nondim]
-beta = R0*k
-print("Displacement prefactor = "+str(beta)+" [nondim]")
-# Bulk viscosity [mPa*s]
-mu = 0.887
-print("Bulk viscosity         = "+str(mu)+" [mPa*s]")
+# GLOBAL VARIABLES (FOR WATER-VAPOR)
 # Surface tension [mPa*m]
 gamma = 57.8
-print("Surface tension        = "+str(gamma)+" [mPa*m]")
-# Reference speed [nm/ps]
-U_ref = (gamma/mu)
-print("Reference speed        = "+str(U_ref)+" [nm/ns]")
-# Reference time [ps]
-tau = R0/U_ref
-print("Reference time         = "+str(tau)+" [ns]")
+print("Surface tension       = "+str(gamma)+" [mPa*m]")
+# Bulk viscosity [mPa*s]
+mu = 0.69
+print("Bulk viscosity        = "+str(mu)+" [mPa*s]")
+# Self expl.
+TWOPI = 360
+# Temperature [K]
+T = 300
+# Boltzmann constant [bar*nm^3/K]
+kB = 0.1380649
+# Depth [nm]
+Ly = 4.67650
 
-# Contact angles [deg]
-theta_g_0 = 130.0
-print("Initial c. a           = "+str(theta_g_0)+" [deg]")
-theta_e = 38.8
-print("Equilibrium c. a.      = "+str(theta_e)+" [deg]")
-delta_theta = theta_g_0-theta_e
 
-# Roughness coefficient 'a' [nondim]
-a = 0.7
-# a = 0.0
-print("Roughness coefficient  = "+str(a)+" [nondim]")
-# Corrugation height [nm]
-h = a/k
-print("Corrugation height     = "+str(h)+" [nm]")
+class RoughSubstrate :
 
-# Wenzel
-a2 = a**2
-rough_parameter = (2.0/np.pi) * np.sqrt(a2+1.0) * sc_spc.ellipe(a2/(a2+1.0))
-theta_w = cos_m1( rough_parameter*cos(theta_e) )
+    def __init__(self, l, mu_f, R0, a, theta_g_0_flat, theta_e, Gamma=None) :
 
-# Initial reduced droplet area [nondim]
-A0 = np.pi
-fun_theta = lambda t : ( np.deg2rad(t)/(sin(t)**2) - cot(t) )
-# Initial wetted distance [nondim]
-x0 = np.sqrt( np.pi/fun_theta(theta_g_0) )
-print("Initial c.l. distance  = "+str(x0*R0)+" [nm]")
+        # Corrugation wavelength [nm]
+        print("Corrugation length    = "+str(l)+" [nm]")
+        # Corrugation number [1/nm]
+        self.k = TWOPI/l
+        print("Corrugation number    = "+str(self.k)+" [1/nm]")
+        # Droplet radius [nm]
+        self.R0 = R0
+        print("Droplet radius        = "+str(R0)+" [nm]")
+        # Roughness coefficient 'a' [nondim]
+        self.a = a
+        print("Roughness coefficient = "+str(a)+" [1]")
+        # Initial c.a. on a flat surface [deg]
+        print("Initial c.a. on flat  = "+str(theta_g_0_flat)+" [deg]")
+        # Equilibrium c.a. on a flat surface [deg]
+        self.theta_e = theta_e
+        print("Equilibrium c. a.     = "+str(theta_e)+" [deg]")
+        # Reference time [ns]
+        self.tau = TWOPI*mu_f/(gamma*self.k)
+        print("Reference time        = "+str(self.tau)+" [ns]")
+        # Corrugation height [nm]
+        h = a/self.k
+        print("Corrugation height    = "+str(h)+" [nm]")
 
-# Friction ratio [nondim]
-mu_star = 10.0
-print("Friction ratio mu_f/mu = "+str(mu_star)+" [nm]")
+        ### Conversion from 'micro-to-macro' ###
+        self.m2m = TWOPI/(self.k*R0)
+        ### Wenzel law ###
+        a2 = a**2
+        self.rough_parameter = (2.0/np.pi) * np.sqrt(a2+1.0) * sc_spc.ellipe(a2/(a2+1.0))
+        ### Eq. Wenzel angle ###
+        self.theta_w = cos_m1(self.rough_parameter*cos(theta_e))
+        # Contact angles [deg]
+        self.theta_g_0 = cos_m1(self.rough_parameter*cos(theta_g_0_flat))
+        print("Initial c. a          = "+str(self.theta_g_0)+" [deg]")
 
-# Langevin coefficient (constant)
-Gamma = 0.00616
-# Gamma = 0.01
+        # Initial reduced droplet area [nondim]
+        self.fun_theta = lambda t : ( np.deg2rad(t)/(sin(t)**2) - cot(t) )
+        # Initial wetted distance [nm]
+        self.x0 = R0*np.sqrt( np.pi/self.fun_theta(self.theta_g_0) )
+        print("Initial c.l. distance = "+str(self.x0)+" [nm]")
 
-# Macroscopic angle (given by circular cap)
-theta_try = theta_g_0
-theta_g = lambda x : sc_opt.fsolve(lambda t : ( (x**2)*fun_theta(t)-np.pi ), theta_try)[0]
+        # Macroscopic angle, given by circular cap (input: coordinate rescaled over R0)
+        self.theta_try = self.theta_g_0
+        self.theta_g = lambda x : sc_opt.fsolve(lambda t : ( (x**2)*self.fun_theta(t)-np.pi ), self.theta_try)[0]
+        print('[TEST] : theta_g_0    = '+str(self.theta_g(self.x0/R0))+" [deg]")
+        
+        if Gamma == None :
+            self.Gamma = (2*kB*T)/(Ly*l*10*gamma)
+        else :
+            self.Gamma = Gamma
+        print("Noise (nondim.)       = "+str(self.Gamma)+" [1]")
 
-# Microscopic angle
-phi = lambda x : theta_g(x) + tan_m1(a*cos(beta*x))
+        # Microscopic angle (input: coordinate rescaled over the wavelength)
+        self.phi = lambda x : self.theta_g(self.m2m*x) + tan_m1(self.a*cos(TWOPI*x))
+        # Curvilinear coordinates measure (input: coordinate rescaled over the wavelength)
+        self.dsdx = lambda x : np.sqrt(1.0+(self.a*cos(TWOPI*x))**2)
+        # MKT Potential (input: coordinate rescaled over the wavelength)
+        self.V = lambda x : (cos(self.theta_e)-cos(self.phi(x)))/self.dsdx(x)
 
-# Curvilinear coordinates measure
-dsdx = lambda x : np.sqrt( 1.0 + (a*cos(beta*x))**2 )
 
-# Potential
-# MKT
-V = lambda x : ( cos( theta_e ) - cos( phi(x) ) ) / dsdx(x)
+class EulerMurayama :
 
-# Numerical integration
-# Final time [ps]
-t_fin = 100.0
-t_bin = 0.01
-T_fin = t_fin/tau
-T_bin = t_bin/tau
-dt = 0.1*T_bin
-Nt = int(T_fin/dt)
+    def __init__(self, RS, t_fin, t_bin, M) :
+        print("Final time            = "+str(t_fin)+" [ns]")
+        self.T_fin = t_fin/RS.tau
+        self.T_bin = t_bin/RS.tau
+        self.dt = 0.1*self.T_bin
+        self.Nt = int(self.T_fin/self.dt)
+        print("T_fin (nondim.)       = "+str(self.T_fin)+" [1]")
+        print("dt (nondim.)          = "+str(self.dt)+" [1]")
+        self.M = M
+        print("#replicas             = "+str(M))
 
-print("Time-step              = "+str(dt*tau)+" [ns]")
+    def simulate_ode(self, RS) :
+        self.x_vec = []
+        self.theta_g_vec = []
+        x = RS.k*RS.x0/TWOPI
+        for n in range(self.Nt) :
+            x = x + RS.V(x)*self.dt
+            self.x_vec.append(x)
+            RS.theta_try = RS.theta_g(RS.m2m*x)
+            self.theta_g_vec.append(RS.theta_try)
+        self.x_vec = np.array(self.x_vec)
+        self.t_vec = np.linspace(0.0, (self.Nt-1)*self.dt, self.Nt)
 
-x_vec = []
-theta_g_vec = []
-x = x0
+    def simulate_sde(self, RS) :
+        self.x_ens = np.zeros(self.Nt)
+        self.x2_ens = np.zeros(self.Nt)
+        self.theta_g_ens = np.zeros(self.Nt)
+        self.theta_g2_ens = np.zeros(self.Nt)
+        for m in range(self.M) :
+            print("Sim. replica "+str(m+1))
+            x_vec_m = []
+            theta_g_vec_m = []
+            x = RS.k*RS.x0/TWOPI
+            RS.theta_try = RS.theta_g_0
+            for n in range(self.Nt) :
+                x = x + ( RS.V(x)*self.dt + np.sqrt(RS.Gamma*self.dt)*rng.normal(0.0,1.0)/RS.dsdx(x) )
+                x_vec_m.append(x)
+                RS.theta_try = RS.theta_g(RS.m2m*x)
+                theta_g_vec_m.append(RS.theta_try)
+            x_vec_m = np.array(x_vec_m)
+            theta_g_vec_m = np.array(theta_g_vec_m)
+            self.x_ens += x_vec_m
+            self.x2_ens += x_vec_m*x_vec_m
+            self.theta_g_ens += theta_g_vec_m
+            self.theta_g2_ens += theta_g_vec_m*theta_g_vec_m
+        self.x2_ens /= self.M
+        self.x_ens /= self.M
+        self.x_var = (self.x2_ens-self.x_ens*self.x_ens)
+        self.x_std = np.sqrt(self.x_var)
+        self.x_ste = self.x_std/np.sqrt(self.M)
+        self.theta_g2_ens /= self.M
+        self.theta_g_ens /= self.M
+        self.theta_var = (self.theta_g2_ens-self.theta_g_ens*self.theta_g_ens)
+        self.theta_std = np.sqrt(self.theta_var)
+        self.theta_ste = self.theta_std/np.sqrt(self.M)
 
-for n in range(Nt) :
-    x = x + V(x)*dt/mu_star
-    x_vec.append(x)
-    theta_try = theta_g(x)
-    theta_g_vec.append(theta_try)
 
-x_vec = np.array(x_vec)
-t_vec = np.linspace(0.0, n*dt, Nt)
+def test_plot() :
 
-# Stochastic replicas
-M = 1
-x_ens = np.zeros(Nt)
-theta_g_ens = np.zeros(Nt)
-# Replicas
-for m in range(M) :
-    print("Sim. replica "+str(m+1))
-    x_vec_m = []
-    theta_g_vec_m = []
-    x = x0
-    theta_try = theta_g_0
-    for n in range(Nt) :
-        x = x + ( V(x)*dt + np.sqrt(Gamma*dt)*rng.normal(0.0, 1.0)/dsdx(x) )/mu_star
-        x_vec_m.append(x)
-        theta_try = theta_g(x)
-        theta_g_vec_m.append(theta_try)
-    x_vec_m = np.array(x_vec_m)
-    theta_g_vec_m = np.array(theta_g_vec_m)
-    x_ens += x_vec_m
-    theta_g_ens += theta_g_vec_m
-x_ens /= M
-theta_g_ens /= M
+    RS = RoughSubstrate(l=1,mu_f=10*mu,R0=20,a=0.1,theta_g_0_flat=105.8,theta_e=55.6)
 
-fig1, (ax1, ax2) = plt.subplots(1, 2)
+    EM = EulerMurayama(RS=RS,t_fin=100.0,t_bin=0.5,M=25)
+    EM.simulate_ode(RS)
+    EM.simulate_sde(RS)
 
-ax1.plot(tau*t_vec, R0*x_vec, 'k-', linewidth=3.0)
-ax1.plot(tau*t_vec, R0*x_ens, 'r-', linewidth=2.0, label='stochastic model, '+str(M)+' replicas')
-ax1.set_xlabel('t [ns]', fontsize=30.0)
-ax1.set_ylabel('x [ns]', fontsize=30.0)
-ax1.set_xlim([tau*t_vec[0], tau*t_vec[-1]])
-ax1.tick_params(axis='x', labelsize=25)
-ax1.tick_params(axis='y', labelsize=25)
-ax1.set_title('Contact line position', fontsize=30.0)
+    theta_w = RS.theta_w
+    print("theta_w       = "+str(theta_w)+" [deg]")
+    theta_fin_ode = EM.theta_g_vec[-1]
+    print("theta_fin_ode = "+str(theta_fin_ode)+" [deg]")
+    theta_fin_sde = np.mean(EM.theta_g_ens[int(0.8*EM.Nt):])
+    print("theta_fin_sde = "+str(theta_fin_sde)+" [deg]")
 
-ax2.plot(tau*t_vec, theta_g_vec, 'k-', linewidth=3.0)
-ax2.plot(tau*t_vec, theta_g_ens, 'r-', linewidth=2.0, label='stochastic model, '+str(M)+' replicas')
-ax2.plot(tau*t_vec, theta_w*np.ones(t_vec.shape), 'k--', linewidth=2.5)
-ax2.set_xlabel('t [ns]', fontsize=30.0)
-ax2.set_ylabel(r'$\theta_g$ [deg]', fontsize=30.0)
-ax2.set_xlim([tau*t_vec[0], tau*t_vec[-1]])
-ax2.tick_params(axis='x', labelsize=25)
-ax2.tick_params(axis='y', labelsize=25)
-ax2.set_title('Global contact angle', fontsize=30.0)
+    fig1, (ax1, ax2) = plt.subplots(2, 1)
 
-plt.show()
+    ax1.plot(RS.tau*EM.t_vec, TWOPI*EM.x_vec/RS.k, 'k-', linewidth=3.0)
+    ax1.plot(RS.tau*EM.t_vec, TWOPI*EM.x_ens/RS.k, 'r-', linewidth=2.5)
+    ax1.fill_between(RS.tau*EM.t_vec,TWOPI*(EM.x_ens+EM.x_std)/RS.k,TWOPI*(EM.x_ens-EM.x_std)/RS.k,color='r',alpha=0.5,linewidth=0.0)
+    ax1.set_ylabel(r'$x_{cl}$ [nm]', fontsize=30.0)
+    ax1.set_xlim([RS.tau*EM.t_vec[0], RS.tau*EM.t_vec[-1]])
+    ax1.tick_params(axis='x',which='both',labelbottom=False)
+    ax1.tick_params(axis='y', labelsize=25)
 
-"""
-plt.plot(t_vec, x_vec+r0, 'k--', linewidth=3.0, label='toy model')
-plt.xlabel('t [-1]', fontsize=30.0)
-plt.ylabel('x [-1]', fontsize=30.0)
-plt.xlim([t_vec[0], t_vec[-1]])
-plt.xticks(fontsize=30.0)
-plt.yticks(fontsize=30.0)
-plt.title('Numerical integration, position', fontsize=30.0)
-plt.legend(fontsize=20.0, loc='lower right')
-plt.show()
+    ax2.plot(RS.tau*EM.t_vec, EM.theta_g_vec, 'k-', linewidth=3.0)
+    ax2.plot(RS.tau*EM.t_vec, EM.theta_g_ens, 'r-', linewidth=2.0, label=r'$<x_{cl}>$')
+    ax2.fill_between(RS.tau*EM.t_vec,EM.theta_g_ens+EM.theta_std,EM.theta_g_ens-EM.theta_std,color='r',alpha=0.5,linewidth=0.0)
+    ax2.plot(RS.tau*EM.t_vec, RS.theta_w*np.ones(EM.t_vec.shape), 'b--', linewidth=3, label=r'$\theta_W$')
+    ax2.set_xlabel(r'$t$ [ns]', fontsize=30.0)
+    ax2.set_ylabel(r'$\theta_g$ [deg]', fontsize=30.0)
+    ax2.set_xlim([RS.tau*EM.t_vec[0], RS.tau*EM.t_vec[-1]])
+    ax2.legend(fontsize=25)
+    ax2.tick_params(axis='x', labelsize=25)
+    ax2.tick_params(axis='y', labelsize=25)
 
-plt.plot(t_vec, theta_g_ens, 'r-', linewidth=2.0, label='stochastic model, '+str(M)+' replicas')
-plt.plot(t_vec, theta_g_vec, 'k--', linewidth=3.0, label='toy model')
-plt.plot(t_vec, theta_w*np.ones(t_vec.shape), 'b-', linewidth=2.5, label='Wenzel')
-plt.xlabel('t [-1]', fontsize=30.0)
-plt.ylabel(r'$\theta_g$ [deg]', fontsize=30.0)
-plt.xlim([t_vec[0], t_vec[-1]])
-plt.xticks(fontsize=30.0)
-plt.yticks(fontsize=30.0)
-plt.title('Numerical integration, global angle', fontsize=30.0)
-plt.legend(fontsize=20.0, loc='upper right')
-plt.show()
-"""
+    plt.show()
+
+
+def parametric_study(l_vec,a_vec,mu_f=10*mu,R0=20,theta_g_0_flat=105.8,theta_e=55.6,t_fin=100.0,t_bin=0.5,M=25) :
+
+    s = (len(l_vec),len(a_vec))
+    theta_w_vec = np.zeros(s)
+    theta_fin_ode_vec = np.zeros(s)
+    theta_fin_sde_vec = np.zeros(s)
+
+    n = 0
+    for i in range(len(l_vec)) :
+        for j in range(len(a_vec)) :
+            n += 1
+            print("[ PROGRESS "+str(n)+"/"+str(theta_w_vec.size)+" ]")
+            RS = RoughSubstrate(l=l_vec[i],mu_f=mu_f,R0=R0,a=a_vec[j],theta_g_0_flat=theta_g_0_flat,theta_e=theta_e)
+            EM = EulerMurayama(RS=RS,t_fin=t_fin,t_bin=t_bin,M=M)
+            EM.simulate_ode(RS)
+            EM.simulate_sde(RS)
+            theta_w = RS.theta_w
+            print("theta_w       = "+str(theta_w)+" [deg]")
+            theta_fin_ode = EM.theta_g_vec[-1]
+            print("theta_fin_ode = "+str(theta_fin_ode)+" [deg]")
+            theta_fin_sde = np.mean(EM.theta_g_ens[int(0.8*EM.Nt):])
+            print("theta_fin_sde = "+str(theta_fin_sde)+" [deg]")
+            print("# ######################################## #")
+            theta_w_vec[i,j] = theta_w
+            theta_fin_ode_vec[i,j] = theta_fin_ode
+            theta_fin_sde_vec[i,j] = theta_fin_sde
+
+    diff_ode = np.abs(theta_fin_ode_vec-theta_w_vec)
+    diff_sde = np.abs(theta_fin_sde_vec-theta_w_vec)
+
+    np.save('diff_ode.npy',diff_ode)
+    np.save('diff_sde.npy',diff_sde)
+
+if __name__ == "__main__" :
+    
+    Np = 36
+    l_vec = np.linspace(0.5,5.5,Np)
+    print(l_vec)
+    a_vec = np.linspace(0,1,Np)
+    print(a_vec)
+    np.save('l_vec.npy',l_vec)
+    np.save('a_vec.npy',a_vec)
+
+    # Testing
+    l_vec = np.load('l_vec.npy')
+    a_vec = np.load('a_vec.npy')
+
+    L, A = np.meshgrid(l_vec,a_vec,sparse=False,indexing='ij')
+
+    # parametric_study(l_vec,a_vec,M=25)
+
+    d1 = np.load('diff_ode.npy')
+    d2 = np.load('diff_sde.npy')
+    vmax = max(np.max(d1),np.max(d2))
+    fig1, (ax1, ax2) = plt.subplots(1, 2)
+    dmap1 = ax1.pcolormesh(L,A,d1,vmin=0,vmax=vmax,cmap=cm.bone)
+    ax1.set_xlabel('l [nm]')
+    ax1.set_ylabel('a [1]')
+    cb1 = plt.colorbar(dmap1,ax=ax1)
+    cb1.ax.set_ylabel(r'$|\theta_{\infty}-\theta_W|$', rotation=270)
+    dmap2 = ax2.pcolormesh(L,A,d2,vmin=0,vmax=vmax,cmap=cm.bone)
+    ax2.set_xlabel('l [nm]')
+    ax2.set_ylabel('a [1]')
+    cb2 = plt.colorbar(dmap2,ax=ax2)
+    cb2.ax.set_ylabel(r'$|\theta_{\infty}-\theta_W|$', rotation=270)
+    plt.show()
+
+    plt.pcolormesh(L,A,d1-d2,cmap=cm.bone)
+    plt.colorbar()
+    plt.show()
---
+++ energy_landscape.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+cos = lambda t : np.cos(np.deg2rad(t))
+sin = lambda t : np.sin(np.deg2rad(t))
+sin2 = lambda t : sin(t)*sin(t)
+
+theta0 = 90
+theta = np.linspace(theta0-30,theta0+30,501)
+
+fig1, (ax1, ax2, ax3) = plt.subplots(3, 1)
+
+F_flat = (theta-theta0)*(cos(theta0)-cos(theta))
+ax1.plot(theta,F_flat,'k--',linewidth=2)
+ax2.plot(theta,F_flat,'k--',linewidth=2)
+ax3.plot(theta,F_flat,'k--',linewidth=2)
+
+eps = 0.1
+a = 500/(2*np.pi)
+F_rough = (theta-theta0)*(cos(theta0)-cos(theta)) + eps*sin2(a*theta)
+ax1.plot(theta,F_rough,'k-',linewidth=3)
+ax1.tick_params(axis='both',labelbottom=False,labelleft=False)
+
+eps = 10
+a = 150/(2*np.pi)
+F_rough = (theta-theta0)*(cos(theta0)-cos(theta)) + eps*sin2(a*theta)
+ax2.plot(theta,F_rough,'k-',linewidth=3)
+ax2.tick_params(axis='both',labelbottom=False,labelleft=False)
+ax2.set_ylabel('Wetting energy landscape',fontsize=30,labelpad=20)
+
+eps = 2
+a = 250/(2*np.pi)
+F_rough = (theta-theta0)*(cos(theta0)-cos(theta)) + eps*sin2(a*theta)
+ax3.plot(theta,F_rough,'k-',linewidth=3)
+ax3.tick_params(axis='both',labelbottom=False,labelleft=False)
+ax3.set_xlabel(r'$\theta_0$',fontsize=30,labelpad=20)
+
+plt.show()
